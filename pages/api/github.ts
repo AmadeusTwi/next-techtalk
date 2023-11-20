69487383268288d09e91ca5cbcb3e624d0c9f4c0
Maksim Valiantsiuk <amadeustwi@gmail.com>
2023-11-20 16:43:06 +0100

    update environment variables and add GitHub token

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -4,7 +4,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await fetch("https://api.github.com/repos/vercel/next.js");
+  const token = process.env.GITHUB_TOKEN;
+  const headers = {
+    Authorization: `token ${token}`,
+  };
+  const response = await fetch("https://api.github.com/repos/vercel/next.js", {
+    headers,
+  });
   const data = await response.json();
 
   res.status(200).json({ stars: data.stargazers_count });
